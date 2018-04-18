3511ea2b6f971302636c8e07bd4105096c152259
Tung Vcc <ngoctung2810@gmail.com>
2018-04-18 09:09:53 +0700

    first commit

---
+++ assets/js/jquery.unav.js
@@ -0,0 +1,45 @@
+/*
+ * The jQuery plugin "uNav" v0.0.1
+ *
+ * https://github.com/Ins-V/uNav#readme
+ *
+ * Copyright (c) 2017 Insane
+ *
+ * Licensed under MIT:
+ *   https://github.com/Ins-V/uNav/blob/master/LICENSE.md
+ *
+ */
+
+(function($)
+{
+    $.fn.uNav = function(options)
+    {
+        return this.each(function(){
+            var navbar = $(this);
+            var nav = navbar.find('nav');
+            var burger = navbar.find('> button');
+            var items = nav.find('> a, > div');
+
+            navbar.addClass('unav');
+            burger.addClass('unav-burger');
+            items.addClass('unav-item');
+            $('div.unav-item').addClass('unav-dropdown');
+            $('.unav-dropdown > div').addClass('unav-dropdown-menu');
+
+            burger.on('click', function(){
+                $('nav').toggleClass('unav-sidebar-show');
+                $('.unav-burger').toggleClass('unav-burger-open');
+            });
+
+            $('.unav-dropdown').on('click', function(e){
+                e.preventDefault();
+                $(this).toggleClass('is-active');
+                $(this).find('.unav-dropdown-menu').toggleClass('is-active')
+                                                   .on('click', function(e){
+                                                        e.stopPropagation();
+                                                   });
+            });
+        });
+    }
+
+})(jQuery);
